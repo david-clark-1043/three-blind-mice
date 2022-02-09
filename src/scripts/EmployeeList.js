8cb678d92b20811bf203a72310781c9772970787
David Clark <david.clark.1043@gmail.com>
2022-02-09 15:36:57 -0600

    Added getting department functionality

--- src/scripts/EmployeeList.js
+++ src/scripts/EmployeeList.js
@@ -1,4 +1,4 @@
-import { getEmployees, getComputers } from "./dataAccess.js";
+import { getEmployees, getComputers, getDepartments } from "./dataAccess.js";
 
 
 // function that builds html string
@@ -6,6 +6,7 @@ export const EmployeeList = () => {
     // get needed data
     const employees = getEmployees()
     const computers = getComputers()
+    const departments = getDepartments()
 
     // initialize empty string
     let html = ""
@@ -13,19 +14,35 @@ export const EmployeeList = () => {
     // iterate over employees and add to the string
     html += employees.map(employee => {
         let employeeHTML = '<div class="employee">'
+
         // add employee name
         employeeHTML += `<header class="employee__name">
-                            <h1>${employee.name}</h1>
+                            <h1>${employee.firstName} ${employee.lastName}</h1>
                         </header>`
+
         // get the employee's computer
         const computer = computers.find(computer => computer.id === employee.computerId)
+        
         // add employee computer
         employeeHTML += `<section class="employee__computer">
                             Currently using a ${computer.year} ${computer.model}
                         </section>`
 
-        return employeeHTML
-    }) 
+        //get the employee's department
+        const department = departments.find(department => department.id === employee.departmentId)
 
+        // add employee department
+        employeeHTML += `<section class="employee__department">
+                            Works in the ${department.name} department
+                        </section>`
+
+        // close employee div tag
+        employeeHTML += "</div>"
+
+        return employeeHTML
+
+    }).join("") // join so single string
+
+    // return final list string
     return html
 }
