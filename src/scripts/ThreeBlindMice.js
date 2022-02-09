58152b91f9806916617756f60b5bbc4a154f6fe2
David Clark <david.clark.1043@gmail.com>
2022-02-09 15:03:13 -0600

    Added employee list

---
+++ src/scripts/ThreeBlindMice.js
@@ -0,0 +1,9 @@
+import { EmployeeList } from "./EmployeeList.js";
+
+// adds all the html together
+
+export const ThreeBlindMice = () => {
+    return `<article class="EmployeeList">
+                ${EmployeeList}
+            </article>`
+}
